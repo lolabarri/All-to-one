0827486d54e57949bcc1180e7dd404f28ca1b78f
Leticia Olábarri <leticiaolabarri@yahoo.es>
2018-12-19 18:10:16 +0100

    Car location is updated with the Finish trip button

--- public/javascripts/trips.js
+++ public/javascripts/trips.js
@@ -1,28 +1,30 @@
 let tripId;
 const startTrip = car => {
-  axios.post(`/trips/start/${car}`, {}).then(trip => { console.log(trip);
-    document.getElementById("isFree").innerText="Is Free?: false";
-    document.getElementById("newTrip").style.display="none";
-    document.getElementById("finishTrip").style.display="block";
-    tripId=trip.data._id;
-  })
+  axios.post(`/trips/start/${car}`, {}).then(trip => {
+    console.log(trip);
+    document.getElementById("isFree").innerText = "Is Free?: false";
+    document.getElementById("newTrip").style.display = "none";
+    document.getElementById("finishTrip").style.display = "block";
+    tripId = trip.data._id;
+  });
 };
 
-const finishTrip = () => {
-  console.log(tripId);
-  axios.post(`/trips/finish/${tripId}`, {}).then(trip => { console.log(trip);
-    document.getElementById("isFree").innerText="Is Free?: true";
-    document.getElementById("newTrip").style.display="block";
-    document.getElementById("finishTrip").style.display="none";
-  })
-};
-
-const saveLocation = car => {
+const finishTrip = car => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+    navigator.geolocation.getCurrentPosition(function(position) {
       var center = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      axios.post(`/trips/location/${car}`, {location: center}).then(car => console.log(car))
-})}}
+      axios
+        .post(`/trips/location/${car}`, { location: center })
+        .then(car => console.log(car));
+      axios.post(`/trips/finish/${tripId}`, {}).then(trip => {
+        console.log(trip);
+        document.getElementById("isFree").innerText = "Is Free?: true";
+        document.getElementById("newTrip").style.display = "block";
+        document.getElementById("finishTrip").style.display = "none";
+      });
+    });
+  }
+};
